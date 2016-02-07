b530408542d5821fd1929070ce3b08bea87eb70e
Hans Laessig <snwboard333@gmail.com>
2016-02-07 16:19:54 -0500

    Login, Sign Up and Reset Password all working

---
+++ StudyBuddy/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  StudyBuddy
+//
+//  Created by Hans Laessig on 2/7/16.
+//  Copyright © 2016 IST440. All rights reserved.
+//
+
+import Parse
+import UIKit
+
+class HomeViewController: UIViewController
+{
+    
+}
